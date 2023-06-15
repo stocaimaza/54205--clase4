51fe0a3535818e3039054a91e7bf724b2dee8065
stocaimaza <stocaimaza@hotmail.com>
2023-06-15 14:02:11 -0300

    clase 4

---
+++ src/componentes/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ItemListContainer = ({greeting}) => {
+  return (
+    <h2 style={{color:"blue", textAlign:"center"}}> {greeting} </h2>
+  )
+}
+
+export default ItemListContainer
